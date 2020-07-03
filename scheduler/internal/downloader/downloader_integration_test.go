4d99810d7e2bb24c511e353d6c1fdd4b51320ed7
horahoradev <horahora1567@gmail.com>
2020-07-02 23:25:12 -0700

    Better test coverage

---
+++ scheduler/internal/downloader/downloader_integration_test.go
@@ -0,0 +1,55 @@
+package downloader
+
+import (
+	"context"
+	"github.com/horahoradev/horahora/scheduler/internal/config"
+	"github.com/horahoradev/horahora/scheduler/internal/models"
+	log "github.com/sirupsen/logrus"
+	"sync"
+	"testing"
+	"time"
+)
+
+// Tests whether we can succeed at downloading something, upload to videoservice, and return when context is cancelled
+func TestConcurrentVideoDownloads(t *testing.T) {
+	conf, err := config.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dlChan := make(chan *models.VideoDlRequest, 10)
+
+	dlSuccessChannel := make(chan Video, 5)
+	downloader := New(dlChan, "./", conf.Client, 5, dlSuccessChannel)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	wg := &sync.WaitGroup{}
+
+	go func() {
+		wg.Add(1)
+		err := downloader.SubscribeAndDownload(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		wg.Done()
+	}()
+
+	dlChan <- models.NewVideoDlRequest(models.Niconico, models.Tag, "Oldest_Video", "1", conf.Conn, conf.Redlock)
+
+	timeoutTicker := time.Tick(5 * time.Minute)
+
+	select {
+	case <-timeoutTicker:
+		log.Fatal("wait for successful download timed out")
+
+	case <-dlSuccessChannel:
+	}
+
+	// Cancel context, causing downloader to return after it's done with the current video download
+	cancel()
+
+	wg.Wait()
+	<-dlSuccessChannel
+}
---
+++ video_service/internal/models/model_test.go
@@ -0,0 +1,80 @@
+package models
+
+import (
+	"context"
+	proto "github.com/horahoradev/horahora/user_service/protocol"
+	"github.com/horahoradev/horahora/video_service/internal/config"
+	videoproto "github.com/horahoradev/horahora/video_service/protocol"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"testing"
+)
+
+var v *VideoModel
+
+func init() {
+	cfg, err := config.New()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Register user
+	req := proto.RegisterRequest{
+		Email:    "wow@wow.com",
+		Username: "wow",
+		Password: "securepassword",
+	}
+
+	for i := 0; i < 11; i++ {
+		_, err = cfg.UserClient.Register(context.Background(), &req)
+		if err != nil {
+			log.Panic(err)
+		}
+	}
+
+	v, err = NewVideoModel(cfg.SqlClient, cfg.UserClient, cfg.RedisConn)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	_, err = v.SaveForeignVideo(context.Background(), "mytestvideo", "wow", "", "0",
+		0, "", "testlocation", "newLoc", []string{"test"}, 10)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func TestSaveForeignVideoAndObtainVideoList(t *testing.T) {
+
+	videoList, err := v.GetVideoList(videoproto.SortDirection_desc, 0)
+	assert.NoError(t, err)
+
+	var videoListContainsVideo bool
+	for _, val := range videoList {
+		if val.VideoTitle == "mytestvideo" {
+			videoListContainsVideo = true
+		}
+	}
+
+	assert.Equal(t, true, videoListContainsVideo)
+}
+
+func TestRating(t *testing.T) {
+	// Ensure that we can add ratings
+	err := v.AddRatingToVideoID("0", "0", 10.0)
+	assert.NoError(t, err)
+
+	// Ensure that the video info reflects the rating that we added
+	info, err := v.getBasicVideoInfo(10, "0")
+	assert.NoError(t, err)
+	assert.Equal(t, 10.0, info.rating)
+}
+
+func TestViews(t *testing.T) {
+	err := v.IncrementViewsForVideo("0")
+	assert.NoError(t, err)
+
+	info, err := v.getBasicVideoInfo(10, "0")
+	assert.NoError(t, err)
+	assert.Equal(t, 1, info.views)
+}
