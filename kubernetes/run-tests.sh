62089967a52c3e43299bc096800c4cf30c5e2a9a
horahoradev <horahora1567@gmail.com>
2020-07-02 23:24:28 -0700

    Better test coverage for videoservice

--- kubernetes/run-tests.sh
+++ kubernetes/run-tests.sh
@@ -3,3 +3,4 @@ set -e -x -o pipefail
 
 # 1. Integration tests
 kubectl exec $(kubectl get pods | grep "scheduler" | awk '{print $1}') -- /bin/bash -c "go test --mod=vendor -timeout=10m -tags=integration ./..."
+kubectl exec $(kubectl get pods | grep "videoservice" | awk '{print $1}') -- /bin/bash -c "cd internal/models/ && go test ."
