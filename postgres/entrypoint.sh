6539adc8caaa3179808008647b40dee3f664629e
ivan k <ivan@sq10.net>
2021-05-25 20:14:08 -0500

    localdev: fully self-contained postgres setup

---
+++ postgres/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+# Run database setup after postgres starts (meaning, this runs in the background)
+{
+  until pg_isready -U $POSTGRES_USER; do
+    echo "Waiting for postgres to start..."
+    sleep 1
+  done
+
+  echo "Creating databases"
+  # TODO(ivan): how do we want to make this so it runs only once
+  psql --user=$POSTGRES_USER -a -f /postgres/create_db.sql
+
+  echo "Running migrations"
+  flyway -user=$POSTGRES_USER -password=$POSTGRES_PASSWORD -url=jdbc:postgresql://localhost:5432/userservice -locations=filesystem:///postgres/user_service/migrations migrate
+  flyway -user=$POSTGRES_USER -password=$POSTGRES_PASSWORD -url=jdbc:postgresql://localhost:5432/scheduler -locations=filesystem:///postgres/scheduler/migrations migrate
+  flyway -user=$POSTGRES_USER -password=$POSTGRES_PASSWORD -url=jdbc:postgresql://localhost:5432/videoservice -locations=filesystem:///postgres/video_service/migrations migrate
+
+  echo "Seeding databases"
+  # TODO(ivan): how do we want to make this so it runs only once
+  psql --user=$POSTGRES_USER -a -f /postgres/seed_db.sql
+} &
+
+exec docker-entrypoint.sh postgres
