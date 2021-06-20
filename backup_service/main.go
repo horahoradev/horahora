49c9880f81e6e21296331a8acbe8a9b6cf4b713a
horahoradev <horahora1567@gmail.com>
2021-06-19 17:09:44 -0700

    WIP backup service

---
+++ backup_service/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"exec"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/horahoradev/horahora/backup_service/internal/config"
+	"github.com/horahoradev/horahora/video_service/storage"
+)
+
+func main() {
+	conf, err := config.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	storageAPI, err := storage.NewB2(conf.BackblazeID, conf.backblazeAPIKey,"otomads")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for true {
+		backupAllDBs(conf, storageAPI)
+		time.Sleep(time.Hour)
+	}
+}
+
+func backupAllDBs(c *config.Config, storageAPI *storage.B2Storage) error {
+	err := dumpAndWrite("userservice", c.UserPGSDatabase, c.UserPGSUsername, c.UserPGSPassword, storageAPI)
+	if err != nil {
+		return err
+	}
+
+	err = dumpAndWrite("scheduler", c.SchedulerPGSDatabase, c.SchedulerPGSUsername, c.SchedulerPGSPassword, storageAPI)
+	if err != nil {
+		return err
+	}
+
+	err = dumpAndWrite("videoservice", c.VideoPGSDatabase, c.VideoPGSUsername, c.VideoPGSPassword, storageAPI)
+	if err != nil {
+		return err
+	}
+}
+
+func dumpAndWrite(hostname, dbName, username, password string, b2 storage.B2Storage) error {
+	filename := fmt.Sprintf("%s-%d", dbName, time.Now().Unix())
+	
+	dumpFile, err := os.Create(fmt.Sprintf("/tmp/%s", filename)
+	if err != nil {
+		return err
+	}
+	defer dumpFile.Close()
+
+	cmd := exec.Command("/usr/bin/pg_dump", []string{
+		fmt.Sprintf("--dbname=postgresql://%s:%s@%s:5432/%s", username, password, hostname, dbName),
+	})
+
+	cmd.Stdout = dumpFile
+
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	err = b2.Upload(dumpFile.Name(), filename)
+	if err != nil {
+		return err
+	}
+
+}
