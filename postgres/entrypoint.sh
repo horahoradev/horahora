36521fbcbd3d8c22e0f17ad1d33d6a2a859674e1
horahoradev <horahora1567@gmail.com>
2021-07-11 16:00:42 -0700

    Added dumb sleep to dodge postgres restart for migrations

--- postgres/entrypoint.sh
+++ postgres/entrypoint.sh
@@ -3,10 +3,13 @@ set -euo pipefail
 
 # Run database setup after postgres starts (meaning, this runs in the background)
 {
-  until pg_isready -U $POSTGRES_USER; do
+  until pg_isready -h localhost -U $POSTGRES_USER; do
     echo "Waiting for postgres to start..."
     sleep 1
   done
+
+  # dodge the post-init restart lol
+  sleep 20
 
   echo "Creating databases"
   # TODO(ivan): how do we want to make this so it runs only once
