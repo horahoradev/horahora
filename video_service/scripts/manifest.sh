5e3d52d3f834078d559962d532473dbf8e4bbe5b
horahoradev <horahora1567@gmail.com>
2021-07-15 19:20:16 -0700

    WIP H264 experiment

--- video_service/scripts/manifest.sh
+++ video_service/scripts/manifest.sh
@@ -4,8 +4,8 @@ set -e -x -o pipefail
   # http://wiki.webmproject.org/adaptive-streaming/instructions-to-playback-adaptive-webm-using-dash was used as a reference
 
 ffmpeg \
- -f webm_dash_manifest -i ${1}_320x240_600k.webm \
- -f webm_dash_manifest -i ${1}_640x360_1000k.webm \
+ -f webm_dash_manifest -i ${1}_320x240_600k.mp4 \
+ -f webm_dash_manifest -i ${1}_640x360_1000k.mp4 \
  -f webm_dash_manifest -i ${1}_audio_128k.webm \
  -c copy -map 0 -map 1 -map 2 \
  -f webm_dash_manifest \
