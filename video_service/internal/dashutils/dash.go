f8385f0e2813a83ff7eb500f21d69b3a5800fec4
horahoradev <horahora1567@gmail.com>
2021-07-15 19:20:16 -0700

    WIP H264 experiment

--- video_service/internal/dashutils/dash.go
+++ video_service/internal/dashutils/dash.go
@@ -3,9 +3,10 @@ package dashutils
 
 import (
 	"fmt"
-	log "github.com/sirupsen/logrus"
 	"os/exec"
 	"path/filepath"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type DASHVideo struct {
@@ -16,19 +17,19 @@ type DASHVideo struct {
 }
 
 func TranscodeAndGenerateManifest(path string, local bool) (*DASHVideo, error) {
-	var encodeArgs []string
-	switch local {
-	case true:
-		// make the encoding really fast so we don't have to wait 90 minutes for integration tests
-		// I don't really understand the difference between the speed and deadline args, but the documentation implies
-		// they're separate
-		encodeArgs = []string{path, "-speed 16 -deadline realtime -r 1 -crf 63 -t 10"}
-	case false:
-		// -r 24 -deadline realtime -cpu-used 1
-		encodeArgs = []string{path, "-r 24 -deadline good -cpu-used 2"}
-	}
+	// var encodeArgs []string
+	// switch local {
+	// case true:
+	// 	// make the encoding really fast so we don't have to wait 90 minutes for integration tests
+	// 	// I don't really understand the difference between the speed and deadline args, but the documentation implies
+	// 	// they're separate
+	// 	encodeArgs = []string{path, "-speed 16 -deadline realtime -r 1 -crf 63 -t 10"}
+	// case false:
+	// 	// -r 24 -deadline realtime -cpu-used 1
+	// 	encodeArgs = []string{path, "-r 24 -deadline good -cpu-used 2"}
+	// }
 
-	cmd := exec.Command("/videoservice/scripts/transcode.sh", encodeArgs...)
+	cmd := exec.Command("/videoservice/scripts/transcode.sh", []string{path}...)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Errorf("%s", out)
