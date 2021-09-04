e9bc557c5bed7f09567b322f89fc6d3f1e94eb21
horahoradev <horahora1567@gmail.com>
2021-09-03 20:57:01 -0700

    Major refactor, part of the way there

--- scheduler/internal/models/video_dl_request.go
+++ scheduler/internal/models/video_dl_request.go
@@ -7,16 +7,10 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-type Category struct {
-	Website      int
-	ContentType  string
-	ContentValue string
-}
 
 type VideoDLRequest struct {
 	Redsync     *redsync.Redsync
 	Db          *sqlx.DB
-	C           Category
 	VideoID     string // Foreign ID
 	ID          int    // Domestic ID
 	DownloaddID int
@@ -24,16 +18,23 @@ type VideoDLRequest struct {
 }
 
 func (v *VideoDLRequest) SetDownloaded() error {
+	website, err := getWebsiteFromURL(v.URL)
+	if err != nil {
+		return err
+	}
+
 	sql := "UPDATE videos SET download_id = $1, upload_time =  Now() WHERE video_ID = $2 AND id IN (select videos.id FROM videos " +
 		"INNER JOIN downloads_to_videos ON videos.id = downloads_to_videos.video_id INNER JOIN downloads ON downloads_to_videos.download_id = downloads.id " +
 		"WHERE videos.website = $3)"
-	_, err := v.Db.Exec(sql, v.DownloaddID, v.ID, v.C.Website)
+	_, err = v.Db.Exec(sql, v.DownloaddID, v.ID, website)
 	if err != nil {
 		return err
 	}
 
 	return v.SetDownloadSucceeded()
 }
+
+
 
 func (v *VideoDLRequest) SetDownloadSucceeded() error {
 	sql := "UPDATE videos SET dlStatus = 1 WHERE id = $1"
---
+++ scheduler/migrations/V5__url_download_url_field.sql
@@ -0,0 +1,1 @@
+ALTER TABLE downloads ADD COLUMN url varchar(255) NOT NULL UNIQUE;
