85d47b3b8d78a50379bb1dfc1f1d440af8738121
horahoradev <horahora1567@gmail.com>
2021-09-24 18:48:25 -0700

    Added web_api to docker compose file, fixed dockerfile

--- front_api/routes/POST_archiverequests.go
+++ front_api/routes/POST_archiverequests.go
@@ -12,9 +12,7 @@ import (
 )
 
 func (r RouteHandler) handleArchiveRequest(c echo.Context) error {
-	website := c.FormValue("website")
-	contentType := c.FormValue("contentType")
-	contentValue := c.FormValue("contentValue")
+	urlVal := c.FormValue("url")
 
 	userID := c.Get(custommiddleware.UserIDKey)
 	UserIDInt, ok := userID.(int64)
@@ -23,48 +21,14 @@ func (r RouteHandler) handleArchiveRequest(c echo.Context) error {
 		return errors.New("could not assert userid to int64")
 	}
 
-	websiteEnumVal, ok := schedulerproto.SupportedSite_value[website]
-	if !ok {
-		return errors.New("site not found")
+	req := schedulerproto.URLRequest{
+		UserID: UserIDInt,
+		Url: urlVal,
 	}
 
-	supportedWebsite := schedulerproto.SupportedSite(websiteEnumVal)
-
-	// FIXME: this is dumb. Fix this to use schedulerproto consts after switching to string instead of enum
-	switch contentType {
-	case "tag":
-		req := schedulerproto.TagRequest{
-			UserID:   UserIDInt,
-			Website:  supportedWebsite, // FIXME: placeholder, see above
-			TagValue: contentValue,
-		}
-
-		_, err := r.s.DlTag(context.TODO(), &req)
-		if err != nil {
-			return err
-		}
-	case "channel":
-		req := schedulerproto.ChannelRequest{
-			Website:   supportedWebsite,
-			ChannelID: contentValue,
-		}
-
-		_, err := r.s.DlChannel(context.TODO(), &req)
-		if err != nil {
-			return err
-		}
-	case "playlist":
-		req := schedulerproto.PlaylistRequest{
-			Website:    supportedWebsite,
-			PlaylistID: contentValue,
-		}
-		_, err := r.s.DlPlaylist(context.TODO(), &req)
-		if err != nil {
-			return err
-		}
-
-	default:
-		return errors.New("invalid content type")
+	_, err := r.s.DlURL(context.TODO(), &req)
+	if err != nil {
+		return err
 	}
 
 	return c.JSON(http.StatusOK, nil)
