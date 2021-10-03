cb84131cfb9917b71012a02303a34fa8f87c808c
horahoradev <horahora1567@gmail.com>
2021-10-03 13:06:38 -0700

    Minor improvements

--- webapp/src/ArchivalPage.js
+++ webapp/src/ArchivalPage.js
@@ -13,14 +13,17 @@ import Paginatione from "./Pagination";
 
 // {"PaginationData":{"PathsAndQueryStrings":["/users/1?page=1"],"Pages":[1],"CurrentPage":1},"UserID":1,"Username":"【旧】【旧】電ǂ鯨","ProfilePictureURL":"/static/images/placeholder1.jpg","Videos":[{"Title":"YOAKELAND","VideoID":1,"Views":11,"AuthorID":0,"AuthorName":"【旧】【旧】電ǂ鯨","ThumbnailLoc":"http://localhost:9000/otomads/7feaa38a-1e10-11ec-a6c3-0242ac1c0004.thumb","Rating":0}]}
 
-function createNewArchival() {
-    const url = document.getElementById('url').value;
-    API.postArchival(url);
-}
 
 function ArchivalPage() {
     const [userData, setUserData] = useState(null);
     const [archivalSubscriptions, setArchivalSubscriptions] = useState([]);
+
+    function createNewArchival() {
+        const url = document.getElementById('url').value;
+        API.postArchival(url);
+        let newList = [...archivalSubscriptions, url];
+        setArchivalSubscriptions(newList);
+    }
 
     // TODO(ivan): Make a nicer page fetch hook that accounts for failure states
     useEffect(() => {
@@ -38,7 +41,7 @@ function ArchivalPage() {
         return () => {
             ignore = true;
         };
-    }, []);
+    }, [archivalSubscriptions]);
 
     let elements = [];
     if (archivalSubscriptions) {
@@ -50,9 +53,12 @@ function ArchivalPage() {
     return (
         <>
             <Header userData={userData} />
-            <input type="text" id="url">
+            <br></br>
+            Type URL to archive here: <input type="text" id="url">
             </input>
-            <button onClick={createNewArchival}>Submit</button>
+            <p></p>
+            <button className="background-blue" onClick={createNewArchival}>Submit</button>
+            <p></p>
             {elements}
             </>
     );
