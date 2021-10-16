080cda4a72a66bdbc21dccd7c6d52fe45a732d74
horahoradev <horahora1567@gmail.com>
2021-10-16 13:15:16 -0700

    Fix broken tailwind CSS

--- webapp/entrypoint.sh
+++ webapp/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-cd /webapp && npm install
+cd /webapp && npm install && npm install -D tailwindcss@npm:@tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9 && npm install @craco/craco
+
 cd /webapp && yarn start
