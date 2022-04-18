9a2eb9c97dab7f8a5227864c10a9b9d309c67280
horahoradev <horahora1567@gmail.com>
2022-04-17 17:01:04 -0700

    Custom JWT mint keypair

---
+++ front_api/middleware/resp_cache.go
@@ -0,0 +1,127 @@
+package middleware
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+
+	"bytes"
+
+	ttlcache "github.com/jellydator/ttlcache/v2"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+)
+
+type ResponseCache struct {
+	cache *ttlcache.Cache
+}
+
+func NewRespCache() *ResponseCache {
+	cache := ttlcache.NewCache()
+
+	cache.SetTTL(time.Duration(5 * time.Minute))
+
+	return &ResponseCache{
+		cache: cache,
+	}
+}
+
+// https://github.com/labstack/echo/blob/master/middleware/body_dump.go was used as a reference
+// Thank you for your work!
+/*
+	The MIT License (MIT)
+
+	Copyright (c) 2021 LabStack
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
+type bodyDumpResponseWriter struct {
+	io.Writer
+	http.ResponseWriter
+}
+
+func (w *bodyDumpResponseWriter) WriteHeader(code int) {
+	w.ResponseWriter.WriteHeader(code)
+}
+
+func (w *bodyDumpResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+func (w *bodyDumpResponseWriter) Flush() {
+	w.ResponseWriter.(http.Flusher).Flush()
+}
+
+func (w *bodyDumpResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	return w.ResponseWriter.(http.Hijacker).Hijack()
+}
+
+func (j *ResponseCache) RespCache(next echo.HandlerFunc) echo.HandlerFunc {
+
+	return func(c echo.Context) error {
+		id := c.Param(UserIDKey)
+
+		idInt, err := strconv.ParseInt(id, 10, 64)
+		if err != nil {
+			log.Error(err)
+		}
+
+		cacheKey := fmt.Sprintf("%d.%s?%s", idInt, c.Request().URL.Path, c.QueryString())
+
+		if c.Request().Method == "GET" {
+			resp, err := j.cache.Get(cacheKey)
+			if err != ttlcache.ErrNotFound {
+				resp, ok := resp.(*bytes.Buffer)
+				if !ok {
+					// dangerous FIXME
+					log.Fatal("could not cast to echo response")
+				}
+
+				log.Infof("CACHE HIT: %v", resp)
+
+				c.Response().Write(resp.Bytes())
+				return nil
+			} else {
+				// cache miss
+				resBody := new(bytes.Buffer)
+				mw := io.MultiWriter(c.Response().Writer, resBody)
+				writer := &bodyDumpResponseWriter{Writer: mw, ResponseWriter: c.Response().Writer}
+				c.Response().Writer = writer
+
+				err := next(c)
+				if err != nil {
+					return err
+				}
+
+				j.cache.Set(cacheKey, resBody)
+
+				log.Infof("CACHE MISS, SET: %v", resBody)
+				return nil
+
+			}
+		}
+
+		return next(c)
+	}
+}
--- webapp/entrypoint.sh
+++ webapp/entrypoint.sh
@@ -3,4 +3,5 @@ set -euo pipefail
 
 cd /webapp && npm install && npm install -D tailwindcss@npm:@tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9 && npm install @craco/craco
 
+# TODO pay attention to linting lol
 cd /webapp && yarn start
