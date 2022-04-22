c517d867f6f0abd4a908e287e8d96382c94322aa
horahoradev <horahora1567@gmail.com>
2022-04-22 12:59:04 -0700

    Registration

---
+++ webapp/src/RegisterPage.js
@@ -0,0 +1,100 @@
+import { useFormik } from "formik";
+import { Button, Input } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faKey, faMailBulk, faUser } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useRef } from "react";
+import { useHistory } from "react-router-dom";
+
+import Header from "./Header";
+import * as API from "./api";
+
+function RegistrationForm() {
+  let history = useHistory();
+  // TODO(ivan): validation, form errors
+  // TODO(ivan): submitting state
+  let formik = useFormik({
+    initialValues: {
+      email: "",
+      username: "",
+      password: "",
+    },
+    onSubmit: async (values) => {
+      await API.postRegister(values);
+      history.push("/");
+    },
+  });
+
+  // automatically focus input on first input on render
+  let usernameInputRef = useRef();
+  useEffect(() => {
+    usernameInputRef.current && usernameInputRef.current.focus();
+  }, [usernameInputRef]);
+
+  return (
+    <div className="max-w-xs w-full border rounded shadow bg-white p-4">
+      <h2 className="text-xl mb-4">Register</h2>
+      <form onSubmit={formik.handleSubmit}>
+      <Input.Group>
+          <Input
+            name="email"
+            values={formik.values.email}
+            onChange={formik.handleChange}
+            size="large"
+            placeholder="email"
+            ref={usernameInputRef}
+            prefix={
+              <FontAwesomeIcon className="mr-1 text-gray-400" icon={faUser} />
+            }
+          />
+          </Input.Group>
+        <Input.Group>
+          <Input
+            name="username"
+            values={formik.values.username}
+            onChange={formik.handleChange}
+            size="large"
+            placeholder="Username"
+            ref={usernameInputRef}
+            prefix={
+              <FontAwesomeIcon className="mr-1 text-gray-400" icon={faUser} />
+            }
+          />
+        </Input.Group>
+        <br />
+        <Input.Group>
+          <Input.Password
+            name="password"
+            values={formik.values.username}
+            onChange={formik.handleChange}
+            size="large"
+            placeholder="Password"
+            prefix={
+              <FontAwesomeIcon className="mr-1 text-gray-400" icon={faKey} />
+            }
+          />
+        </Input.Group>
+        <br />
+        <Input.Group>
+          <Button block type="primary" htmlType="submit" size="large">
+            Submit
+          </Button>
+        </Input.Group>
+      </form>
+    </div>
+  );
+}
+
+function RegisterPage() {
+  return (
+    <>
+      <Header dataless />
+      <div className="flex justify-center mx-4">
+        <div className="max-w-screen-lg w-screen my-6 flex justify-center items-center pt-32">
+          <RegistrationForm />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default RegisterPage;
