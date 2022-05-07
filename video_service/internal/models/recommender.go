b70de4c0a62fb1b0c8f0e0eb1944f81cd56305aa
horahoradev <horahora1567@gmail.com>
2022-05-07 15:42:48 -0700

    Recommender bugfixes

--- video_service/internal/models/recommender.go
+++ video_service/internal/models/recommender.go
@@ -79,8 +79,8 @@ func (b *BayesianTagSum) RemoveRecommendedVideoForUser(userID, videoID int64) er
 func (b *BayesianTagSum) getRecommendations(uid int64) ([]*videoproto.VideoRec, error) {
 	// Videos which have been viewed and not rated are implicitly rated 0
 	// left join from video scores returns some random videos by default
-	sql := "WITH tag_ratings AS (select tag, coalesce(avg(ratings.rating), 0.00) AS tag_score from ratings LEFT JOIN tags ON ratings.video_id = tags.video_id WHERE ratings.user_id = $1 AND tag_score >= 3.5 GROUP BY tag), " +
-		"video_scores AS (SELECT ratings.video_id, coalesce(avg(tag_score), 0.00) AS video_score from  tags INNER JOIN tag_ratings ON tag_ratings.tag = tags.tag GROUP BY ratings.video_id ORDER BY video_score DESC, ratings.video_id LIMIT 50) " +
+	sql := "WITH tag_ratings AS (select tag, coalesce(avg(ratings.rating), 0.00) AS tag_score from ratings INNER JOIN tags ON ratings.video_id = tags.video_id WHERE ratings.user_id = $1 GROUP BY tag), " +
+		"video_scores AS (SELECT tags.video_id, coalesce(avg(tag_score), 0.00) AS video_score from  tags INNER JOIN tag_ratings ON tag_ratings.tag = tags.tag WHERE tag_score >= 3.5 GROUP BY tags.video_id ORDER BY video_score DESC, tags.video_id LIMIT 50) " +
 		"SELECT videos.id, title, newLink from video_scores INNER JOIN videos ON video_scores.video_id = videos.id WHERE videos.is_deleted IS false AND videos.transcoded IS true AND videos.id NOT IN (SELECT video_id FROM ratings WHERE ratings.user_id = $1) limit 10"
 	rows, err := b.db.Query(sql, uid)
 	if err != nil {
