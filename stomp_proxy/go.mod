d24bb7b423ce534ed2c1565fe631325353c7a5db
horahoradev <horahora1567@gmail.com>
2022-06-04 15:52:18 -0700

    Added stomp proxy

---
+++ stomp_proxy/go.mod
@@ -0,0 +1,14 @@
+module github.com/horahoradev/horahora/stomp_proxy
+
+go 1.17
+
+require (
+	github.com/go-stomp/stomp/v3 v3.0.5
+	github.com/gorilla/websocket v1.5.0
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require (
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+)
---
+++ stomp_proxy/main.go
@@ -0,0 +1,115 @@
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"flag"
+	"net/http"
+	"time"
+
+	frame "github.com/go-stomp/stomp/v3/frame"
+	"github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
+)
+
+var addr = flag.String("addr", "0.0.0.0:15674", "http service address")
+
+var upgrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }} // use default options
+
+var cstDialer = websocket.Dialer{
+	ReadBufferSize:   4096,
+	WriteBufferSize:  4096,
+	HandshakeTimeout: 30 * time.Second,
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Errorf("upgrade: %v", err)
+		return
+	}
+
+	rabbitConn, _, err := cstDialer.Dial("ws://rabbitmq:15674/ws", nil)
+	if err != nil {
+		log.Errorf("amqp dial: %v", err)
+		return
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// client <- rabbitmq
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				msgType, msg, err := rabbitConn.ReadMessage()
+				if err != nil {
+					log.Errorf("reader: %v", err)
+					return
+				}
+
+				writer, err := c.NextWriter(msgType)
+				if err != nil {
+					log.Errorf("nextwriter: %v", err)
+					return
+				}
+
+				_, err = writer.Write(msg)
+				if err != nil {
+					writer.Close()
+					log.Errorf("write: %v", err)
+					return
+				}
+				writer.Close()
+			}
+		}
+	}()
+
+	// client -> rabbitmq
+	for {
+		mt, wsReader, err := c.NextReader()
+		if err != nil {
+			log.Errorf("ws reader: %v", err)
+			return
+		}
+
+		wsWriter, err := rabbitConn.NextWriter(mt)
+		if err != nil {
+			log.Errorf("ws writer: %v", err)
+			return
+		}
+
+		frameReader := frame.NewReader(wsReader)
+		if err != nil {
+			log.Errorf("new reader: %v", err)
+			return
+		}
+
+		frameMsg, err := frameReader.Read()
+		if err != nil {
+			log.Errorf("frame reader: %v", err)
+			return
+		}
+
+		log.Infof("write %v", frameMsg)
+		frameWriter := frame.NewWriter(wsWriter)
+		err = frameWriter.Write(frameMsg)
+		if err != nil {
+			wsWriter.Close()
+			log.Errorf("frame write: %v", err)
+			return
+		}
+		wsWriter.Close()
+	}
+
+}
+
+func main() {
+	http.HandleFunc("/ws", echo)
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
