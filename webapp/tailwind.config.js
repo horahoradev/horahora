1ba94908069f9a3cf49bb589a02c17b6dc4f979b
gabengar <gabengar@protonmail.com>
2022-06-20 20:55:32 +0500

    chore: tailwind update

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -1,19 +1,12 @@
-module.exports = {
-  darkMode: 'class',
+/** @type {import('tailwindcss').Config} */
+const tailwindConfig = {
+  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   important: true,
-  mode: "jit",
-  purge: [
-    "./public/**/*.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require("nightwind"),
-  ],
-  
+  plugins: [require("nightwind")],
 };
+
+module.exports = tailwindConfig;
