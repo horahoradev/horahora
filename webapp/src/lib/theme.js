acab2b12210778d687b0714fba67fb08182c0efa
gabengar <gabengar@protonmail.com>
2022-06-26 23:38:21 +0500

    feat: theme lib

---
+++ webapp/src/lib/theme.js
@@ -0,0 +1,31 @@
+import { getCookie, setCookie } from "../store/cookie";
+
+/**
+ * @typedef {"dark" | "light"} ITheme
+ */
+
+export const DEFAULT_THEME = "dark";
+
+export function getTheme() {
+  /**
+   * @typedef {ITheme}
+   */
+  let currentTheme = undefined;
+
+  try {
+    currentTheme = getCookie("theme");
+  } catch (error) {
+    currentTheme = document.documentElement.dataset.theme;
+    setCookie("theme", currentTheme);
+  }
+
+  return currentTheme;
+}
+
+/**
+ * @param {ITheme} theme
+ */
+export function setTheme(theme) {
+  document.documentElement.dataset.theme = theme;
+  setCookie("theme", theme);
+}
