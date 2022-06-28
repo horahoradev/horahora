1ee44496b9e058e5997a3554913e0b420887fe42
gabengar <gabengar@protonmail.com>
2022-06-29 01:45:09 +0500

    chore: postcss config tweak

--- webapp/postcss.config.js
+++ webapp/postcss.config.js
@@ -1,21 +1,27 @@
+const IS_PRODUCTION = process.env.NODE_ENV === "production";
+
 const postCSSConfig = {
-  plugins:
-    process.env.NODE_ENV === "production"
-      ? {
-          "tailwindcss": {},
-          "postcss-preset-env": {
-            autoprefixer: {
-              flexbox: "no-2009",
-            },
-            stage: 3,
-            features: {
-              "custom-properties": false,
-            },
-          },
-        }
-      : {
-          "tailwindcss": {},
-        },
+  plugins: {
+    tailwindcss: {},
+  },
 };
+
+if (IS_PRODUCTION) {
+  const prodPlugins = {
+    "postcss-flexbugs-fixes": {},
+    "postcss-preset-env": {
+      autoprefixer: {
+        flexbox: "no-2009",
+        grid: "autoplace"
+      },
+      stage: 3,
+      features: {
+        "custom-properties": false,
+      },
+    },
+  };
+
+  postCSSConfig.plugins = { ...postCSSConfig.plugins, ...prodPlugins };
+}
 
 module.exports = postCSSConfig;
