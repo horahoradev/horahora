e124aa91a6fd6564ddb6b5aef64d4fb4436619f3
gabengar <gabengar@protonmail.com>
2022-06-29 17:49:30 +0500

    chore: user page

--- webapp/src/pages/users/[user_id].tsx
+++ webapp/src/pages/users/[user_id].tsx
@@ -5,36 +5,50 @@ import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { UserOutlined } from "@ant-design/icons";
 import { useRouter } from "next/router";
 
-import * as API from "../../api";
-import { Header } from "../../components/header";
-import { UserRank } from "../../api/types";
-import { VideoList } from "../../components/video-list";
-import Paginatione from "../../components/pagination";
+import {
+  getUser,
+  getUserdata,
+  banUser,
+  setUserMod,
+  setUserAdmin,
+} from "#api/index";
+import { Header } from "#components/header";
+import { UserRank } from "#api/types";
+import { VideoList } from "#components/video-list";
+import Paginatione from "#components/pagination";
 
 // {"PaginationData":{"PathsAndQueryStrings":["/users/1?page=1"],"Pages":[1],"CurrentPage":1},"UserID":1,"Username":"【旧】【旧】電ǂ鯨","ProfilePictureURL":"/static/images/placeholder1.jpg","Videos":[{"Title":"YOAKELAND","VideoID":1,"Views":11,"AuthorID":0,"AuthorName":"【旧】【旧】電ǂ鯨","ThumbnailLoc":"http://localhost:9000/otomads/7feaa38a-1e10-11ec-a6c3-0242ac1c0004.thumb","Rating":0}]}
 
+interface IPageUserData extends Record<string, unknown> {
+  L: Record<string, unknown>;
+  Videos: Record<string, unknown>[];
+  UserID: number;
+}
+
 function UsersPage() {
   const router = useRouter();
-  const { query, isReady } = router
+  const { query, isReady } = router;
   const { user_id } = query;
 
   const [userData, setUserData] = useState(null);
-  const [pageUserData, setPageUserData] = useState({});
+  // @ts-expect-error typing
+  const [pageUserData, setPageUserData] = useState<IPageUserData>({});
   const [currPage, setPage] = useState(1);
 
   // TODO(ivan): Make a nicer page fetch hook that accounts for failure states
   useEffect(() => {
     if (!isReady) {
-      return
+      return;
     }
 
     let ignore = false;
 
     let fetchData = async () => {
-      let pageUserData = await API.getUser(user_id, currPage);
+      // @ts-expect-error some types
+      let pageUserData = await getUser(user_id, currPage);
       if (!ignore) setPageUserData(pageUserData);
 
-      let userData = await API.getUserdata();
+      let userData = await getUserdata();
       if (!ignore) setUserData(userData);
     };
 
@@ -59,10 +73,7 @@ function UsersPage() {
         </h1>
         {pageUserData.L && pageUserData.L.rank === UserRank.ADMIN && (
           <h1 className={"flex justify-center"}>
-            <Button
-              type="primary"
-              onClick={() => API.banUser(pageUserData.UserID)}
-            >
+            <Button type="primary" onClick={() => banUser(pageUserData.UserID)}>
               Ban
             </Button>
           </h1>
@@ -71,7 +82,7 @@ function UsersPage() {
           <h1 className={"flex justify-center"}>
             <Button
               type="primary"
-              onClick={() => API.setUserMod(pageUserData.UserID)}
+              onClick={() => setUserMod(pageUserData.UserID)}
             >
               Promote to mod
             </Button>
@@ -81,7 +92,7 @@ function UsersPage() {
           <h1 className={"flex justify-center"}>
             <Button
               type="primary"
-              onClick={() => API.setUserAdmin(pageUserData.UserID)}
+              onClick={() => setUserAdmin(pageUserData.UserID)}
             >
               Promote to admin
             </Button>
@@ -91,7 +102,10 @@ function UsersPage() {
         {/* TODO: no more copy pasta! */}
         <div className="flex justify-center mx-4 min-h-screen">
           <div className="max-w-screen-lg w-screen my-6">
-            <VideoList videos={pageUserData.Videos} />
+            <VideoList
+              // @ts-expect-error some type
+              videos={pageUserData.Videos}
+            />
             <Paginatione
               paginationData={pageUserData.PaginationData}
               onPageChange={setPage}
