640f3dbe5692eaccafb02ea4e3cb4acdc7b07031
gabengar <gabengar@protonmail.com>
2022-06-29 17:39:25 +0500

    chore: api

--- webapp/environment/vars.ts
+++ webapp/environment/vars.ts
@@ -1,4 +1,4 @@
-export const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
+export const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT!;
 
 if (API_ENDPOINT == null) {
   throw new Error("No NEXT_PUBLIC_API_ENDPOINT provided!");
