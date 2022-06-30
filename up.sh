1342418ca5e41fb05b6a2fabcdf1093c8545d783
gabengar <gabengar@protonmail.com>
2022-07-01 03:49:54 +0500

    chore: prod compose

--- up.sh
+++ up.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+set -euo pipefail
 
-./generate.sh && \
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose build --parallel --progress=plain && \
+# copy example env file if it doesn't exist
+if [ ! -f .env ]
+then
+  cp configs/.env.example .env
+fi
+
+# docker compose by default reads `.env` file
+# so no need to pass it as an option
+docker-compose build --progress=plain && \
 docker-compose up -d
