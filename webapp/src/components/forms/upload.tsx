728c0bfaf7b583ffe2096272dcea48e4c9581f7a
gabengar <gabengar@protonmail.com>
2022-07-03 20:16:34 +0500

    feat: basic upload form schema

--- webapp/src/components/forms/upload.tsx
+++ webapp/src/components/forms/upload.tsx
@@ -1,1 +1,7 @@
-export function uploadForm() {}
+import type { FormEvent } from "react";
+
+export function UploadForm() {
+  async function handleFileUpload(event: FormEvent<HTMLFormElement>) {}
+
+  return <form onSubmit={handleFileUpload}></form>;
+}
--- webapp/src/pages/account/upload.tsx
+++ webapp/src/pages/account/upload.tsx
@@ -1,11 +1,14 @@
+import { UploadForm } from "#components/forms/upload";
 import { Page } from "#components/page";
 
 function UploadPage() {
-  return <Page>
-    <div className="flex justify-center mx-4">
-      
-    </div>
-  </Page>;
+  return (
+    <Page>
+      <div className="flex justify-center mx-4">
+        <UploadForm/>
+      </div>
+    </Page>
+  );
 }
 
 export default UploadPage;
