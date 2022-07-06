2103aae263f3bfc87aef18d1dee71de99ede5a73
gabengar <gabengar@protonmail.com>
2022-07-06 22:12:33 +0500

    feat: local link component

--- webapp/src/components/links/_index.ts
+++ webapp/src/components/links/_index.ts
@@ -2,3 +2,5 @@ export { LinkExternal } from "./external";
 export type { ILinkExternalProps } from "./external";
 export { LinkInternal } from "./internal";
 export type { ILinkInternalProps } from "./internal";
+export { LinkLocal } from "./local";
+export type { ILinkLocalProps as ILinkLocal } from "./local";
