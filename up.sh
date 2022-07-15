5a97bac3cbde50c9a0ca74f7d16eb1183ec0c60d
GabenGar <87906913+GabenGar@users.noreply.github.com>
2022-07-15 08:55:40 +0500

    refactor: development compose config (#65)
    * chore: vscode stuff
    
    * feat: front_api dev dockerfile
    
    compose file was renamed as per spec:
    https://docs.docker.com/compose/compose-file/#compose-file
    
    * feat: scheduler dev image
    
    * feat: stomp proxy dev image
    
    * feat: user service dev image
    
    * feat: video service dev image
    
    * chore: restart directives
    
    * feat: database dev image
    
    * feat: activemq dev iamge
    
    * feat: webapp and minio dev images
    
    * chore: build errors
    
    * fix: build errors

--- up.sh
+++ up.sh
@@ -29,4 +29,4 @@ fi
 # docker compose by default reads `.env` file
 # so no need to pass it as an option
 docker-compose build --progress=plain && \
-docker-compose up -d
+docker-compose up --build -d
