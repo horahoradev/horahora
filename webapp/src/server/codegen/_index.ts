6d68df50af484277355c0eaed49a2de72419a2ab
gabengar <gabengar@protonmail.com>
2022-07-18 18:31:22 +0500

    feat: codegen lib

--- webapp/src/server/codegen/_index.ts
+++ webapp/src/server/codegen/_index.ts
@@ -2,6 +2,58 @@
   Codegen is only run in development,
   therefore a nodejs server context.
 */
-const codegenFolder = ["src", "codegen"]
 
-export {}
+import path from "node:path";
+import fs from "node:fs/promises";
+import { cwd } from "node:process";
+
+import { reduceFolder } from "#server/lib";
+
+export interface ICodegenModule extends Record<string, unknown> {
+  default: () => Promise<string>;
+}
+
+const codegenFolder = ["src", "codegen"];
+const generatorFilename = "generator";
+const generatorModule = `${generatorFilename}.ts`;
+const resultFilename = "result.ts";
+
+export async function runCodegen() {
+  const codegenPath = path.join(cwd(), ...codegenFolder);
+
+  const codegenDirs = await reduceFolder<string[]>(
+    codegenPath,
+    [],
+    async (codegenDirs, entryPath, dirEntry) => {
+      if (!dirEntry.isFile() || dirEntry.name !== generatorModule) {
+        return codegenDirs;
+      }
+
+      const modulePath = path.format(entryPath);
+      const { default: codegenFunction, ...libExports }: ICodegenModule =
+        await import(modulePath);
+      const imports = [
+        "import {",
+        Object.keys(libExports).join(", "),
+        `} from "./${generatorFilename}"`,
+      ].join("\n");
+      const resultString = await codegenFunction();
+      const resultPath = path.join(entryPath.dir, resultFilename);
+      const finalResult = [
+        "/*",
+        "  This module was created by a codegen, do not edit it manually.",
+        "*/",
+        imports,
+        resultString,
+        "\n",
+      ].join("\n");
+      await fs.writeFile(resultPath, finalResult);
+
+      codegenDirs.push(path.relative(codegenPath, entryPath.dir));
+
+      return codegenDirs;
+    }
+  );
+
+  return codegenDirs;
+}
--- webapp/src/server/lib/reduce-folder.ts
+++ webapp/src/server/lib/reduce-folder.ts
@@ -1,9 +1,10 @@
 import { readdir } from "node:fs/promises";
-import { Dirent } from "node:fs";
-import path from "node:path";
+import { type Dirent } from "node:fs";
+import path, { type ParsedPath } from "node:path";
 
 export type IWalkFunc<InitType> = (
   accumulator: InitType,
+  parsedPath: ParsedPath,
   dirEntry: Dirent
 ) => Promise<typeof accumulator>;
 
@@ -12,28 +13,40 @@ export type IWalkFunc<InitType> = (
  * on each file and folder within it,
  * visiting all nested folders.
  * @TODO less aggressive recursion.
+ * @param rootFolder A folder to start walking from.
  */
 export async function reduceFolder<InitType = unknown>(
   rootFolder: string,
   initValue: InitType,
   walkFunc: IWalkFunc<InitType>
 ): Promise<typeof initValue> {
-  const rootEntries = await readdir(rootFolder, { withFileTypes: true });
-  let accumulator: InitType = initValue;
+  try {
+    const rootEntries = await readdir(rootFolder, { withFileTypes: true });
+    let accumulator: InitType = initValue;
 
-  for await (const dirEntry of rootEntries) {
-    // run callback on a file
-    if (dirEntry.isFile()) {
-      accumulator = await walkFunc(accumulator, dirEntry);
-      continue;
+    for await (const dirEntry of rootEntries) {
+      const entryPath = path.join(rootFolder, dirEntry.name);
+      const parsedPath = path.parse(entryPath);
+      // run callback on a file
+      if (dirEntry.isFile()) {
+        accumulator = await walkFunc(accumulator, parsedPath, dirEntry);
+        continue;
+      }
+
+      // run the function itself on a folder
+      if (dirEntry.isDirectory()) {
+        accumulator = await reduceFolder(entryPath, accumulator, walkFunc);
+      }
     }
 
-    // run the function itself on a folder
-    if (dirEntry.isDirectory()) {
-      const newRootFolder = path.join(rootFolder, dirEntry.name);
-      accumulator = await reduceFolder(newRootFolder, accumulator, walkFunc);
+    return accumulator;
+  } catch (error) {
+    const isProperError = error instanceof Error;
+
+    if (!isProperError) {
+      throw error;
     }
+
+    throw new Error(`Failed to walk folder ${rootFolder}`, { cause: error });
   }
-
-  return accumulator;
 }
