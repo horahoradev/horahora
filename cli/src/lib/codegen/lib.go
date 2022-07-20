738568b5fdd7c2f5c486a33cba2a0294638f3b76
gabengar <gabengar@protonmail.com>
2022-07-20 18:19:34 +0500

    chore: shema collection

--- cli/src/lib/codegen/lib.go
+++ cli/src/lib/codegen/lib.go
@@ -3,10 +3,15 @@ package codegen
 import (
 	"fmt"
 	"horahora/cli/src/lib/errors"
+	fslib "horahora/cli/src/lib/fs"
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 )
+
+// Key is the schema ID while the value is the schema literal
+type ISchemaCollection = map[string][]byte
 
 const (
 	schemaFolder      string = "schema"
@@ -14,14 +19,32 @@ const (
 )
 
 var (
+	schemaCollection   ISchemaCollection
 	metaSchemaFilename = fmt.Sprintf("meta%v", schemaFilenameEnd)
 	schemaFolderPath   string
 )
 
-func CollectJSONSchemas() {
-	filepath.WalkDir(schemaFolderPath, func(path string, enry fs.DirEntry, err error) error {
+func CollectJSONSchemas() *ISchemaCollection {
+	if len(schemaCollection) != 0 {
+		return &schemaCollection
+	}
+
+	filepath.WalkDir(schemaFolderPath, func(path string, entry fs.DirEntry, err error) error {
+		if !isSchemaFile(entry) {
+			return nil
+		}
+
+		fslib.ReadFile(path)
+
 		return nil
 	})
+
+	return &schemaCollection
+}
+
+func isSchemaFile(entry fs.DirEntry) bool {
+	// exclusing metaschema from the collection for now
+	return entry.Type().IsRegular() && entry.Name() != metaSchemaFilename && strings.HasSuffix(entry.Name(), schemaFilenameEnd)
 }
 
 func init() {
