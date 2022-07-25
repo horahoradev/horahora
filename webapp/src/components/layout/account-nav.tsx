beb2d1f3c8b4ff81142269199cc85c736c524d23
gabengar <gabengar@protonmail.com>
2022-07-25 08:27:24 +0500

    style: account nav

--- webapp/src/components/layout/account-nav.tsx
+++ webapp/src/components/layout/account-nav.tsx
@@ -1,14 +1,15 @@
 import {
   faArchive,
-  faBars,
   faKey,
   faSignOutAlt,
   faUser,
   faUpload,
+  faArrowRightToBracket,
+  faUserPlus,
 } from "@fortawesome/free-solid-svg-icons";
 import { useState, useEffect } from "react";
+import clsx from "clsx";
 
-import styles from "./account-nav.module.scss";
 import { ThemeSwitcher } from "./theme-switcher";
 
 import { getUserdata } from "#api/index";
@@ -17,30 +18,64 @@ import { UserRank } from "#api/types";
 import { ListItem, ListUnordered } from "#components/lists";
 import { Button } from "#components/buttons";
 import { type ILoggedInUserData } from "#codegen/schema/001_interfaces";
+import { onParentBlur } from "#lib/dom";
+
+// eslint-disable-next-line
+import styles from "./account-nav.module.scss";
 
 export function AccountNavigation() {
+  const [isLoading, switchLoading] = useState(true);
+  const [isOpen, switchOpen] = useState(false);
   const [userData, setUserData] = useState<ILoggedInUserData>();
+
+  const className = clsx(
+    styles.block,
+    isOpen && styles.block_open,
+    isLoading && styles.block_loading
+  );
   const isRegistered = Boolean(userData && userData.username);
   const isAdmin = userData?.rank === UserRank.ADMIN;
 
   useEffect(() => {
     (async () => {
-      let userData = await getUserdata();
-      setUserData(userData);
+      try {
+        let userData = await getUserdata();
+        setUserData(userData);
+      } finally {
+        switchLoading(false);
+      }
     })();
   }, []);
 
   return (
-    <ListItem className={styles.block}>
-      <Button iconID={faBars}>Account</Button>
-      <ListUnordered>
+    <ListItem
+      className={className}
+      onBlur={onParentBlur(() => {
+        switchOpen(false);
+      })}
+    >
+      <Button
+        className={styles.switch}
+        iconID={faUser}
+        onClick={() => {
+          switchOpen(!isOpen);
+        }}
+      >
+        Account
+      </Button>
+      <ListUnordered className={styles.list}>
         {!isRegistered ? (
           <>
             <ListItem>
-              <LinkInternal href="/authentication/login">Login</LinkInternal>
+              <LinkInternal
+                href="/authentication/login"
+                iconID={faArrowRightToBracket}
+              >
+                Login
+              </LinkInternal>
             </ListItem>
             <ListItem>
-              <LinkInternal href="/authentication/register">
+              <LinkInternal href="/authentication/register" iconID={faUserPlus}>
                 Register
               </LinkInternal>
             </ListItem>
