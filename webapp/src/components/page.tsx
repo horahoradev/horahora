46970e5e74a670044482e925c6a8bfdfa91dc86b
gabengar <gabengar@protonmail.com>
2022-07-25 06:40:01 +0500

    feat: page component

--- webapp/src/components/page.tsx
+++ webapp/src/components/page.tsx
@@ -1,9 +1,23 @@
 import { type ReactNode } from "react";
 
+import { Heading } from "#components/headings";
+
 interface IPageProps {
+  title?: ReactNode;
   children: ReactNode;
 }
 
-export function Page({ children }: IPageProps) {
-  return <>{children}</>;
+/**
+ * If no `title` prop is passed, then it's up to the page
+ * to construct and style the page markup.
+ */
+export function Page({ title, children }: IPageProps) {
+  return !title ? (
+    children
+  ) : (
+    <>
+      <Heading level={1}>{title}</Heading>
+      <section>{children}</section>
+    </>
+  );
 }
