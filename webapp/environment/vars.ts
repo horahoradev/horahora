39607d2485a6365d10ba2f8f291668e154adcb8f
gabengar <gabengar@protonmail.com>
2022-08-01 19:14:50 +0500

    chore: env cleanup

--- webapp/environment/vars.ts
+++ webapp/environment/vars.ts
@@ -6,8 +6,3 @@ export const PUBLIC_ORIGIN = process.env.NEXT_PUBLIC_ORIGIN!;
 export const PUBLIC_API_ORIGIN = process.env.NEXT_PUBLIC_PUBLIC_API_ORIGIN!;
 export const PUBLIC_API_PREFIX =
   process.env.NEXT_PUBLIC_PUBLIC_API_PATHNAME_PREFIX!;
-export const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT!;
-
-if (API_ENDPOINT == null) {
-  throw new Error("No NEXT_PUBLIC_API_ENDPOINT provided!");
-}
