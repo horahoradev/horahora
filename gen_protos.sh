24a3aecda3c5661ba1ae28c1956db93af0fde4ac
horahoradev <horahora1567@gmail.com>
2022-09-01 22:13:32 -0700

    envoy protobuf validation

--- gen_protos.sh
+++ gen_protos.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 docker build -t grpcutil .
-docker run -v $(pwd)/video_service:/video_service -v $(pwd)/scheduler:/scheduler -v $(pwd)/user_service:/user_service -it -t grpcutil
+docker run -v $(pwd)/video_service:/video_service -v $(pwd)/scheduler:/scheduler -v $(pwd)/user_service:/user_service -it -t grpcutil /bin/bash
