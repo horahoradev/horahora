5d4a0f1c507e31ccab1b0d2ea96b3dff2599531a
ravihidayat <naufalravi@graduate.utm.my>
2022-09-12 01:13:25 +0700

    Implement Validators (#95)
    * Proto bump
    
    * Implement delete
    
    * Attempt to fix validator.proto import issues
    
    Co-authored-by: horahoradev <horahora1567@gmail.com>

--- gen_all.sh
+++ gen_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-$(cd video_service/protocol && protoc --go_out=. --go_opt=paths=source_relative     --go-grpc_out=. --go-grpc_opt=module=github.com/horahoradev/horahora/video_service/protocol  --validate_opt=module=github.com/horahoradev/horahora/video_service/protocol --validate_out="lang=go:."   videoservice.proto)
-$(cd scheduler/protocol && protoc --go_out=. --go_opt=paths=source_relative   --go-grpc_out=. --go-grpc_opt=module=github.com/horahoradev/horahora/scheduler/protocol  --validate_opt=module=github.com/horahoradev/horahora/scheduler/protocol --validate_out="lang=go:."   scheduler.proto)
-$(cd user_service/protocol && protoc --go_out=. --go_opt=paths=source_relative     --go-grpc_out=. --go-grpc_opt=module=github.com/horahoradev/horahora/user_service/protocol  --validate_opt=module=github.com/horahoradev/horahora/user_service/protocol --validate_out="lang=go:."   userservice.proto)
+$(cd video_service/protocol && protoc -I ${GOPATH}/src --go_out=. --go_opt=paths=source_relative     --go-grpc_out=. --go-grpc_opt=module=github.com/horahoradev/horahora/video_service/protocol  --validate_opt=module=github.com/horahoradev/horahora/video_service/protocol --validate_out="lang=go:."   videoservice.proto)
+$(cd scheduler/protocol && protoc -I ${GOPATH}/src --go_out=. --go_opt=paths=source_relative   --go-grpc_out=. --go-grpc_opt=module=github.com/horahoradev/horahora/scheduler/protocol  --validate_opt=module=github.com/horahoradev/horahora/scheduler/protocol --validate_out="lang=go:."   scheduler.proto)
+$(cd user_service/protocol && protoc -I ${GOPATH}/src --go_out=. --go_opt=paths=source_relative     --go-grpc_out=. --go-grpc_opt=module=github.com/horahoradev/horahora/user_service/protocol  --validate_opt=module=github.com/horahoradev/horahora/user_service/protocol --validate_out="lang=go:."   userservice.proto)
