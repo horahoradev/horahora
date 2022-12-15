b8be6e45602e0acdb940243bad800b62c9981a86
horahoradev <horahora1567@gmail.com>
2022-12-14 17:27:59 -0800

    Add redirect back in

--- webapp/src/lib/fetch/api.ts
+++ webapp/src/lib/fetch/api.ts
@@ -23,9 +23,10 @@ export async function apiFetch<ResBody = never>({
   const response = await fetch(url, { ...options, credentials: "include" });
 
   // Why the hell does the status say 200 if it's a 301??
-  // if (response.redirected) {
-  //   console.log(`Redirecting to new location: ${response.url}`);
-  // }
+  if (response.redirected) {
+    console.log(`Redirecting to new location: ${response.url}`);
+    location.replace(response.url);
+  }
 
   if (!response.ok) {
     // @TODO: 403 status handling
